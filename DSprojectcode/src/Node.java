977eaa7d2cd496762f04ff7ea3a03763f7223717
rahafaleid <rahafaleid@localhost>
2025-04-26 17:28:32 +0300

    rahaf work

--- DSprojectcode/src/Node.java
+++ DSprojectcode/src/Node.java
@@ -1,4 +1,99 @@
 
-public class Node {
+public class Node<T> {
 
+    public T data;
+    public Node<T> next;
+
+    Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedList<T> {
+
+    private Node<T> head;
+    private Node<T> current;
+
+    public LinkedList() {
+        head = current = null;
+    }
+
+    // Check if the list is empty
+    public boolean empty() {
+        return head == null;
+    }
+
+    // Check if current node is the last node
+    public boolean last() {
+        return current.next == null;
+    }
+
+    // Linked list is never full
+    public boolean full() {
+        return false;
+    }
+
+    // Move current to the first node
+    public void findFirst() {
+        current = head;
+    }
+
+    // Move current to the next node
+    public void findNext() {
+        current = current.next;
+    }
+
+    // Retrieve data from the current node
+    public T retrieve() {
+        return current.data;
+    }
+
+    // Update data of the current node
+    public void update(T d) {
+        current.data = d;
+    }
+
+    // Insert a new node after the current node
+    public void insert(T d) {
+        Node<T> newnode = new Node<T>(d);
+        if (head == null) {
+            head = newnode;
+            current = newnode;
+        } else {
+            newnode.next = current.next;
+            current.next = newnode;
+            current = newnode;
+        }
+    }
+
+    // Remove the current node
+    public void remove() {
+        if (head == current) {
+            head = head.next;
+            current = head;
+            return;
+        } else {
+            Node<T> p = head;
+            while (p.next != current)
+                p = p.next;
+            p.next = current.next;
+        }
+
+        if (current != null && current.next != null)
+            current = current.next;
+        else
+            current = head;
+    }
+
+    // Display all nodes in the list
+    public void display() {
+        if (head == null)
+            System.out.println("Empty List");
+        Node<T> p = head;
+        while (p != null) {
+            System.out.println(p.data + "  ");
+            p = p.next;
+        }
+    }
 }
--- DSprojectcode/src/PhotoManger.java
+++ DSprojectcode/src/PhotoManger.java
@@ -1,4 +1,68 @@
 
 public class PhotoManger {
 
-}
+
+	    LinkedList<Photo> allPhotos;
+	    public int tagsNum;
+
+	    // Constructor to initialize the photo manager
+	    public PhotoManager() {
+	        allPhotos = new LinkedList<Photo>();
+	    }
+
+	    // Returns the list of all photos managed
+	    public LinkedList<Photo> getPhotos() {
+	        return allPhotos;
+	    }
+
+	    // Adds a new photo if it doesn't already exist
+	    public void addPhoto(Photo p) {
+	        if (photoExists(p)) {
+	            return; // avoid duplicates
+	        }
+	        allPhotos.insert(p);
+	    }
+
+	    // Deletes a photo based on its path
+	    public void deletePhoto(String path) {
+	        if (allPhotos.empty()) {
+	            return;
+	        }
+
+	        allPhotos.findFirst();
+	        while (!allPhotos.last()) {
+	            if (allPhotos.retrieve().getPath().equals(path)) {
+	                allPhotos.remove();
+	                break;
+	            }
+	            allPhotos.findNext();
+	        }
+
+	        // Check the last photo as well
+	        if (allPhotos.retrieve().getPath().equals(path)) {
+	            allPhotos.remove();
+	        }
+	    }
+
+	    // Helper method to check if a photo already exists
+	    public boolean photoExists(Photo p) {
+	        if (allPhotos.empty()) {
+	            return false;
+	        }
+
+	        allPhotos.findFirst();
+	        while (!allPhotos.last()) {
+	            if (allPhotos.retrieve().getPath().equals(p.getPath())) {
+	                return true;
+	            }
+	            allPhotos.findNext();
+	        }
+
+	        // Check the last photo separately
+	        return allPhotos.retrieve().getPath().equals(p.getPath());
+	    }
+	}
+
+
+
+
